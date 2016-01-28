00772cff8e709d2688c9aeb81a556a604c773c3f
Allen Day <allenday@users.noreply.github.com>
2016-01-28 12:21:23 +0800

    non paired reads specific

--- bin/bowtie_wrapper_single.pl
+++ bin/bowtie_wrapper_single.pl
@@ -1,11 +1,12 @@
 #!/usr/bin/perl
 use strict;
+use Data::Dumper qw(Dumper);
 use File::Temp qw(tempfile);
 use JSON qw(decode_json);
 
 die '$DEMO not defined' unless -d $ENV{'DEMO'};
 
-my $template = "@%s/%d\n%s\n+%s/%d\n%s\n";
+my $template = "@%s\n%s\n+%s\n%s\n";
 
 my ($fh1, $fq1) = tempfile( "fqsplit_XXXXX", SUFFIX => '.fq', UNLINK => 1, DIR => "/tmp" );
 
@@ -15,20 +16,20 @@ while (my $line = <>){
   $line =~ s/^\(/[/;
   $line =~ s/\)$/]/;
   my $pair = decode_json( $line );
-
+  #warn Dumper($pair);
   my $read1 = sprintf( $template,
-    $pair->[0]->{'readName'},
-    1,
-    $pair->[0]->{'sequence'},
-    $pair->[0]->{'readName'},
-    1,
-    $pair->[0]->{'qual'},
+    $pair->{'readName'},
+    $pair->{'sequence'},
+    $pair->{'readName'},
+    $pair->{'qual'},
   );
+
+  print $fh1 $read1;
 }
 
 close( $fh1 );
 
-open( SAM, $ENV{'DEMO'}."/build/bowtie/bowtie ".$ENV{'DEMO'}."/build/data/NC_008253 $fq1 |" );
+open( SAM, $ENV{'DEMO'}."/build/bowtie/bowtie -S ".$ENV{'DEMO'}."/build/data/NC_008253 $fq1 |" );
 while ( my $line = <SAM> ) {
   warn $line;
   print $line;
